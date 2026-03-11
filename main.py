e79cc10d6138b22e4b758a4c2b0d134ee8ae873d
Tran Duc Nguyen_2126 <tdnguyencr2810@gmail.com>
2026-03-11 08:21:05 +0700

    sum a,b

---
+++ main.py
@@ -0,0 +1,17 @@
+nghe
+git remote remove origin
+>> git remote add origin https://github.com/MinhPhine/MinhPhi.git
+>> git push -u origin 20260126
+git remote remove origin git remote add origin https://github.com/MinhPhine/MinhPhi.git
+>> git push -u origin 20260126
+git remote remove origin
+git remote add origin https://github.com/MinhPhine/MinhPhi.git
+git push -u origin 20260126
+# sum a, b
+def sum(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = 10
+    b = 20
+    print("sum of a and b is", sum(a, b))
